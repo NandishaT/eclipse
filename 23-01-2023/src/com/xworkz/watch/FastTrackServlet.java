9fe11a1570c30c1c126ec688bba32ce9d9291c71
unknown <nandishat.xworkz@gmail.com>
2023-01-24 11:04:22 +0530

    23-01-2023

---
+++ 23-01-2023/src/com/xworkz/watch/FastTrackServlet.java
@@ -0,0 +1,25 @@
+package com.xworkz.watch;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(loadOnStartup=1,urlPatterns="/myapps")
+
+public class FastTrackServlet extends HttpServlet {
+	
+	public FastTrackServlet(){
+		System.out.println("created" +this.getClass().getSimpleName());
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("Created FastTrack");
+		
+	}
+
+}
